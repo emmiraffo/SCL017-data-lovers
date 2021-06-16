9744f8c3bb2c86848e325a7f60392ab7c235e14c
Emmi Raffo <emmyraffoc@gmail.com>
2021-06-15 20:58:03 -0400

    Realización inicial de los filtros

--- src/main.js
+++ src/main.js
@@ -2,40 +2,69 @@ import data from './data/lol/lol.js';
 
 import {dropdown} from './data.js';
 
-const button = document.getElementById("dButton");
-    button.addEventListener("click", ()=>{
-    dropdown()
-    });
-
 
 const container = document.getElementById('container');
 const list = Object.values(data.data);
-for (let i=0; i < list.length; i++){
 
-    const champCard = document.createElement('div');
-    champCard.classList.add('champCard');
+//listeners
+const button = document.getElementById("dButton");
+button.addEventListener("click", ()=>{
+    dropdown()
+});
 
-    const name = document.createElement('p');
-    name.classList.add('name');
-    name.textContent = list[i].name;
+function limpiarLista() {
+    while(container.hasChildNodes())
+        container.removeChild(container.firstChild);
+}
 
-    const title = document.createElement('p');
-    title.classList.add('title')
-    title.textContent = list[i].title;
 
-    const img = document.createElement('img');
-    img.src = list[i].img; 
-    img.classList.add('photos');
+function filtrarPorRoles(event) {
+    console.log(event.path[1].dataset.tags)
+    var filterTag = event.path[1].dataset.tags
+    const filteredList = list.filter(function(elementoArray){
+       return elementoArray.tags.includes(filterTag)
+    })
+    console.log(filteredList)
 
+    limpiarLista()
+    dibujarHeroes(filteredList)
+}
+
+document.querySelectorAll(".rol").forEach(function(element) {
+    element.addEventListener("click", filtrarPorRoles)
+});
+
+
+function dibujarHeroes(list) {
+    for (let i=0; i < list.length; i++){
+
+        const champCard = document.createElement('div');
+        champCard.classList.add('champCard');
     
-    champCard.appendChild(img);
-    champCard.appendChild(name);
-    champCard.appendChild(title);
-   
+        const name = document.createElement('p');
+        name.classList.add('name');
+        name.textContent = list[i].name;
+    
+        const title = document.createElement('p');
+        title.classList.add('title')
+        title.textContent = list[i].title;
+    
+        const img = document.createElement('img');
+        img.src = list[i].img; 
+        img.classList.add('photos');
+    
+        
+        champCard.appendChild(img);
+        champCard.appendChild(name);
+        champCard.appendChild(title);
+       
+    
+        container.appendChild(champCard);
+    }
+}
 
-    container.appendChild(champCard);
+dibujarHeroes(list)
 
-};
 
 
 
