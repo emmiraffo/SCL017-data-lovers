5a22ed3e12b5abac2cbe0d001e099722c4ec4ab2
Emmi Raffo <emmyraffoc@gmail.com>
2021-06-28 22:16:57 -0400

    se modificaron nombres de archivos, se comenzaron los test y se creo el archivo hero.js para modularizar funciones

--- src/champion.js
+++ src/champion.js
@@ -10,7 +10,21 @@ console.log(champion)
 const nameChampion = document.getElementById('name');
 const img = document.getElementById('img');
 const tag = document.getElementById('tag');
+const blurb = document.getElementById('blurb');
+const partype = document.getElementById('partype');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const magic = document.getElementById('magic');
+const difficulty = document.getElementById('difficulty');
 
-nameChampion.textContent = champion.name;
+
 img.setAttribute('src',champion.splash) 
-tag.textContent = champion.tags;
+nameChampion.textContent = champion.name;
+blurb.textContent = champion.blurb;
+tag.textContent = "Rol: " + champion.tags;
+partype.textContent = "Tipo de poder : " + champion.partype;
+attack.textContent = "Nivel de ataque : " + champion.info.attack;
+defense.textContent = "Defensa : " + champion.info.defense;
+magic.textContent = "Magia : " + champion.info.magic;
+difficulty.textContent = "Dificultad : " + champion.info['difficulty'];
+console.log("Dificultad : " + champion.info["difficulty"])
